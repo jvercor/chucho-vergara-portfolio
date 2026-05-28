00cefab3f45250a0bba603d73ebf8570cc058b63
jvercor <238141305+jvercor@users.noreply.github.com>
2026-05-27 20:50:46 -0600

    remove search from navbar and frontend

--- src/Header/Nav/index.tsx
+++ src/Header/Nav/index.tsx
@@ -5,8 +5,6 @@ import React from 'react'
 import type { Header as HeaderType } from '@/payload-types'
 
 import { CMSLink } from '@/components/Link'
-import Link from 'next/link'
-import { SearchIcon } from 'lucide-react'
 
 export const HeaderNav: React.FC<{ data: HeaderType }> = ({ data }) => {
   const navItems = data?.navItems || []
@@ -23,10 +21,6 @@ export const HeaderNav: React.FC<{ data: HeaderType }> = ({ data }) => {
           />
         )
       })}
-      <Link href="/search">
-        <span className="sr-only">Search</span>
-        <SearchIcon className="w-5 text-foreground hover:text-primary transition-colors" />
-      </Link>
     </nav>
   )
 }
